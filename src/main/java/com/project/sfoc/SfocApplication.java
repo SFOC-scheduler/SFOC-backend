53f5872115e29aa41e7d7171ff3eb4037291c2e4
PLT134340 <songwoo1015@gmail.com>
2024-01-18 16:54:30 +0900

    chore: 초기 파일 설정

---
+++ src/main/java/com/project/sfoc/SfocApplication.java
@@ -0,0 +1,13 @@
+package com.project.sfoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/sfoc/SfocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.sfoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
