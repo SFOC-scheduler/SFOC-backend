1652d8d654137e964ae9f2b8f5393691ac9b1ce1
PL134340 <songwoo1015@gmail.com>
2024-02-06 16:40:22 +0900

    feat: Google OAuth2 인증 기능 추가

--- src/main/java/com/project/sfoc/config/SecurityConfig.java
+++ src/main/java/com/project/sfoc/config/SecurityConfig.java
@@ -1,25 +1,37 @@
 package com.project.sfoc.config;
 
+import com.project.sfoc.entity.user.*;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Collections;
-
-import static org.springframework.security.config.Customizer.withDefaults;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http,
+            OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService
+    ) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(corsConfigurer -> corsConfigurer.configurationSource(corsConfigurationSource()))
@@ -28,7 +40,10 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/login", "/actuator/**").permitAll()
                         .anyRequest().authenticated()
                 )
-                .formLogin(withDefaults())
+                .oauth2Login(oauth2 -> oauth2
+                        .defaultSuccessUrl("/oauth/loginInfo", true)
+                        .userInfoEndpoint(user -> user.userService(oAuth2UserService))
+                )
                 .logout(logout -> logout.logoutSuccessUrl("/"))
                 .build();
     }
@@ -47,4 +62,38 @@ public class SecurityConfig {
         return source;
     }
 
+    @Bean
+    public OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService(UserService userService) {
+        final DefaultOAuth2UserService delegate = new DefaultOAuth2UserService();
+
+        return userRequest -> {
+            OAuth2User oAuth2User = delegate.loadUser(userRequest);
+            String registrationId = userRequest
+                    .getClientRegistration()
+                    .getRegistrationId();
+            String userNameAttributeName = userRequest.getClientRegistration()
+                    .getProviderDetails()
+                    .getUserInfoEndpoint()
+                    .getUserNameAttributeName();
+
+            Map<String, Object> attributes = oAuth2User.getAttributes(); // OAuth 서비스의 유저 정보들
+
+            UserDto userDto = OAuthAttributes.extract(registrationId, attributes);
+            User user = userService.findByProviderAndSub(userDto.provider(), userDto.sub())
+                    .map(u -> u.update(userDto.email())) // OAuth 서비스 사이트에서 유저 정보 변경이 있을 수 있기 때문에 우리 DB에도 update
+                    .orElse(userDto.toEntity());
+            userService.saveUser(user);
+
+            Map<String, Object> customAttribute = new LinkedHashMap<>();
+            customAttribute.put(userNameAttributeName, attributes.get(userNameAttributeName));
+            customAttribute.put("provider", registrationId);
+            customAttribute.put("email", userDto.email());
+
+            return new DefaultOAuth2User(
+                    Collections.singleton(new SimpleGrantedAuthority("USER")),
+                    customAttribute,
+                    userNameAttributeName);
+
+        };
+    }
 }
