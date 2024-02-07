8ffbda6e65d9e7a948702af0a07e2dc5dc7abbb0
PL134340 <songwoo1015@gmail.com>
2024-02-07 21:11:22 +0900

    feat: CustomOAuth2User, CustomOAuth2UserService 분리, 구현

--- src/main/java/com/project/sfoc/config/SecurityConfig.java
+++ src/main/java/com/project/sfoc/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.project.sfoc.config;
 
-import com.project.sfoc.entity.user.*;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,11 +7,9 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
-import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
@@ -20,8 +17,6 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Collections;
-import java.util.LinkedHashMap;
-import java.util.Map;
 
 @Configuration
 @EnableWebSecurity
@@ -44,6 +39,8 @@ public class SecurityConfig {
                         .defaultSuccessUrl("/oauth/loginInfo", true)
                         .userInfoEndpoint(user -> user.userService(oAuth2UserService))
                 )
+//                .sessionManagement(session -> session
+//                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .logout(logout -> logout.logoutSuccessUrl("/"))
                 .build();
     }
@@ -62,38 +59,4 @@ public class SecurityConfig {
         return source;
     }
 
-    @Bean
-    public OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService(UserService userService) {
-        final DefaultOAuth2UserService delegate = new DefaultOAuth2UserService();
-
-        return userRequest -> {
-            OAuth2User oAuth2User = delegate.loadUser(userRequest);
-            String registrationId = userRequest
-                    .getClientRegistration()
-                    .getRegistrationId();
-            String userNameAttributeName = userRequest.getClientRegistration()
-                    .getProviderDetails()
-                    .getUserInfoEndpoint()
-                    .getUserNameAttributeName();
-
-            Map<String, Object> attributes = oAuth2User.getAttributes(); // OAuth 서비스의 유저 정보들
-
-            UserDto userDto = OAuthAttributes.extract(registrationId, attributes);
-            User user = userService.findByProviderAndSub(userDto.provider(), userDto.sub())
-                    .map(u -> u.update(userDto.email())) // OAuth 서비스 사이트에서 유저 정보 변경이 있을 수 있기 때문에 우리 DB에도 update
-                    .orElse(userDto.toEntity());
-            userService.saveUser(user);
-
-            Map<String, Object> customAttribute = new LinkedHashMap<>();
-            customAttribute.put(userNameAttributeName, attributes.get(userNameAttributeName));
-            customAttribute.put("provider", registrationId);
-            customAttribute.put("email", userDto.email());
-
-            return new DefaultOAuth2User(
-                    Collections.singleton(new SimpleGrantedAuthority("USER")),
-                    customAttribute,
-                    userNameAttributeName);
-
-        };
-    }
 }
---
+++ src/main/java/com/project/sfoc/security/CustomOAuth2User.java
@@ -0,0 +1,43 @@
+package com.project.sfoc.security;
+
+import com.project.sfoc.entity.user.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@RequiredArgsConstructor
+public class CustomOAuth2User implements OAuth2User {
+
+    private final User user;
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        Map<String, Object> attributes = new LinkedHashMap<>();
+        attributes.put("sub", user.getSub());
+        attributes.put("provider", user.getProvider().name());
+        attributes.put("email", user.getEmail());
+        return attributes;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> collection = new ArrayList<>();
+        collection.add(new GrantedAuthority() {
+            @Override
+            public String getAuthority() {
+                return user.getGrant().getRole();
+            }
+        });
+        return collection;
+    }
+
+    @Override
+    public String getName() {
+        return user.getProvider().name() + user.getSub();
+    }
+}
