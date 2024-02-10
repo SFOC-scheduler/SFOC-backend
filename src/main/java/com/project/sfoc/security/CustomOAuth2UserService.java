8d7b6be36156bdb6aaa98e0c32cad45d110673fb
PL134340 <songwoo1015@gmail.com>
2024-02-10 23:28:29 +0900

    refactor: 필요없는 부분 삭제, 디버깅 코드 변경

--- src/main/java/com/project/sfoc/security/CustomOAuth2UserService.java
+++ src/main/java/com/project/sfoc/security/CustomOAuth2UserService.java
@@ -8,6 +8,7 @@ import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -16,6 +17,7 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
     private final UserRepository userRepository;
 
     @Override
+    @Transactional
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         String registrationId = userRequest.getClientRegistration().getRegistrationId();
 
--- src/main/java/com/project/sfoc/security/OAuthController.java
+++ src/main/java/com/project/sfoc/security/OAuthController.java
@@ -2,22 +2,31 @@ package com.project.sfoc.security;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/oauth")
 public class OAuthController {
 
     @GetMapping("/loginInfo")
-    public ResponseEntity<String> oauthLoginInfo(Authentication authentication){
-        OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
-        Map<String, Object> attributes = oAuth2User.getAttributes();
-        return ResponseEntity.ok(attributes.toString());
+    public ResponseEntity<Map<String, Object>> oauthLoginInfo(Authentication authentication){
+        Long id = (Long) authentication.getPrincipal();
+        String role = authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .collect(Collectors.joining(","));
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("id", id);
+        map.put("role", role);
+
+        return ResponseEntity.ok(map);
     }
 
 }
