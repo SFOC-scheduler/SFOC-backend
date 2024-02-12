010649a2822a9af4b6764ca0969283689831a6df
전현민 <77275989+joyjhm@users.noreply.github.com>
2024-02-12 16:55:18 +0900

    feat: TeamMemberRepository 클래스 생성

---
+++ src/main/java/com/project/sfoc/team/TeamMemberRepository.java
@@ -0,0 +1,13 @@
+package com.project.sfoc.team;
+
+import com.project.sfoc.entity.TeamMember;
+import com.project.sfoc.entity.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface TeamMemberRepository extends JpaRepository<TeamMember, Long> {
+
+    boolean existsByTeam(Team team);
+
+    boolean existsByUserNickname(String userNickname);
+}
