9666736d1e0e2eece15a83d77b52a132a78e3b82
전현민 <77275989+joyjhm@users.noreply.github.com>
2024-02-12 16:53:22 +0900

    feat: 팀 참가 controller 추가

--- src/main/java/com/project/sfoc/team/TeamController.java
+++ src/main/java/com/project/sfoc/team/TeamController.java
@@ -1,9 +1,14 @@
 package com.project.sfoc.team;
 
+import com.project.sfoc.entity.TeamGrant;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/teams")
@@ -17,7 +22,24 @@ public class TeamController {
     public ResponseEntity<?> createTeam(@RequestBody TeamRequestDto teamRequestDto) {
         teamService.createTeam(teamRequestDto);
         log.info("팀 생성");
+
         return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/{teamId}/entry")
+    public ResponseEntity<Map<String, Object>> setUserNickname(@RequestBody String userNickname,
+                                                               @PathVariable Long teamId, Authentication authentication) {
+
+        Long userId = (Long) authentication.getPrincipal();
+        TeamGrant teamGrant = teamService.entryTeam(TeamMemberDto.of(userNickname, userId, teamId));
+
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("userId", userId);
+        map.put("teamId", teamId);
+        map.put("userNickname", userNickname);
+        map.put("grant", teamGrant);
+
+        return ResponseEntity.ok(map);
     }
 
 }
