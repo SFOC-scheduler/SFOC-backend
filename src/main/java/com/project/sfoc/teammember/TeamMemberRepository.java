d8adb03b4397e39529b5660f44857ef5e33bc9be
전현민 <77275989+joyjhm@users.noreply.github.com>
2024-02-17 20:54:18 +0900

    refactor: 리팩토링

--- src/main/java/com/project/sfoc/teammember/TeamMemberRepository.java
+++ src/main/java/com/project/sfoc/teammember/TeamMemberRepository.java
@@ -1,6 +1,5 @@
 package com.project.sfoc.teammember;
 
-import com.project.sfoc.entity.user.User;
 import com.project.sfoc.team.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,7 +8,6 @@ import java.util.Optional;
 
 public interface TeamMemberRepository extends JpaRepository<TeamMember, Long> {
 
-    boolean existsByTeam(Team team);
 
     boolean existsByUserNicknameAndTeam(String userNickname, Team team);
 
@@ -18,5 +16,4 @@ public interface TeamMemberRepository extends JpaRepository<TeamMember, Long> {
 
     Optional<TeamMember> findByTeam_IdAndUser_Id(Long teamId, Long userId);
 
-    void deleteByTeam_IdAndUser_Id(Long teamId, Long userId);
 }
