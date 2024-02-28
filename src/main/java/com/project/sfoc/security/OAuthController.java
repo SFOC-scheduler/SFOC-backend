a99642a18078790a8b8803586393c9df4b4a7844
PL134340 <songwoo1015@gmail.com>
2024-02-28 13:11:34 +0900

    feat: 디버깅 엔드포인트 null 참조 예외 처리

--- src/main/java/com/project/sfoc/security/OAuthController.java
+++ src/main/java/com/project/sfoc/security/OAuthController.java
@@ -3,31 +3,22 @@ package com.project.sfoc.security;
 import com.project.sfoc.security.jwt.UserClaims;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/oauth")
 public class OAuthController {
 
     @GetMapping("/loginInfo")
-    public ResponseEntity<Map<String, Object>> oauthLoginInfo(Authentication authentication){
-        UserClaims userClaims = (UserClaims) authentication.getPrincipal();
-        String role = authentication.getAuthorities().stream()
-                .findFirst()
-                .map(GrantedAuthority::getAuthority)
-                .orElse("Anonymous");
-
-        Map<String, Object> map = new HashMap<>();
-        map.put("id", userClaims.id());
-        map.put("role", role);
-
-        return ResponseEntity.ok(map);
+    public ResponseEntity<UserClaims> oauthLoginInfo(Authentication authentication) {
+        try {
+            UserClaims userClaims = (UserClaims) authentication.getPrincipal();
+            return ResponseEntity.ok(userClaims);
+        } catch (Exception e) {
+            return ResponseEntity.ok(UserClaims.of(null, "anonymous"));
+        }
     }
 
 }
