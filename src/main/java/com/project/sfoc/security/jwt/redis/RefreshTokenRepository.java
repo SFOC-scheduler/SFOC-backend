16effa36bf949bde6956576e3041939a1decb961
PL134340 <songwoo1015@gmail.com>
2024-02-28 13:00:20 +0900

    feat: RefreshToken 클래스, 리포지토리, 서비스 추가

---
+++ src/main/java/com/project/sfoc/security/jwt/redis/RefreshTokenRepository.java
@@ -0,0 +1,9 @@
+package com.project.sfoc.security.jwt.redis;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface RefreshTokenRepository extends CrudRepository<RefreshToken, Long> {
+    Optional<RefreshToken> findByContent(String content);
+}
