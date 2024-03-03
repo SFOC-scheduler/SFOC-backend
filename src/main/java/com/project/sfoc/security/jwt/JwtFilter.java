0f55af421f43d7c9dbe5329d9041504234d52d63
PLT134340 <songwoo1015@gmail.com>
2024-03-03 10:55:08 +0900

    refactor: 토큰 검사 및 재발급 분기 리팩토링

--- src/main/java/com/project/sfoc/security/jwt/JwtFilter.java
+++ src/main/java/com/project/sfoc/security/jwt/JwtFilter.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
@@ -17,6 +18,7 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
@@ -31,47 +33,54 @@ public class JwtFilter extends OncePerRequestFilter {
             throws ServletException, IOException {
 
         log.info("접속자 ip={}", request.getRemoteAddr());
+        log.info("요청 URI={}", request.getRequestURI());
 
-        String accessToken = jwtUtil.resolveAccessToken(request).orElse(null);
-        log.info("accessToken={}", accessToken);
+        Optional<String> accessToken = jwtUtil.resolveAccessToken(request);
+        Optional<String> refreshToken = jwtUtil.resolveRefreshToken(request);
 
-        if (accessToken == null) {
-            log.info("인증 실패");
-            filterChain.doFilter(request, response);
-            return;
-        }
+        accessToken.filter(jwtUtil::isNotExpired)
+                .map(jwtUtil::getUserClaims)
+                .map(claims -> createAuthenticationToken(claims, request))
+                .ifPresentOrElse(this::setAuthentication, () -> handleReIssuanceOrFailure(refreshToken, response));
 
+        filterChain.doFilter(request, response);
+    }
+
+    private Authentication createAuthenticationToken(UserClaims claims, HttpServletRequest request) {
+        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                claims,
+                null,
+                List.of(new SimpleGrantedAuthority(claims.role())));
+        authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        return authentication;
+    }
+
+    private void setAuthentication(Authentication authentication) {
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        log.info("인증 완료");
+    }
+
+    private void handleReIssuanceOrFailure(Optional<String> refreshToken, HttpServletResponse response) {
+        refreshToken.filter(jwtUtil::isNotExpired)
+                .filter(refreshTokenService::isValid)
+                .map(jwtUtil::getUserClaims)
+                .map(claims -> jwtUtil.createTokens(claims.id(), claims.role()))
+                .ifPresentOrElse(tokenDto -> handleReIssuance(tokenDto, response), this::handleAuthenticationFailure);
+    }
+
+    private void handleReIssuance(TokenDto tokenDto, HttpServletResponse response){
+        log.info("token 재발급, access={}, refresh={}", tokenDto.accessToken(), tokenDto.refreshToken());
+        response.setHeader(HttpHeaders.SET_COOKIE, jwtUtil.createCookie(JwtUtil.REFRESH_TYPE, tokenDto.refreshToken()));
         try {
-            UserClaims userClaims = jwtUtil.getUserClaims(accessToken);
-            log.info("인증 완료");
-
-            UsernamePasswordAuthenticationToken authenticationToken =
-                    new UsernamePasswordAuthenticationToken(
-                            userClaims,
-                            null,
-                            List.of(new SimpleGrantedAuthority(userClaims.role())));
-
-            authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-        } catch (Exception e1) {
-            String refreshToken = jwtUtil.resolveRefreshToken(request).orElse(null);
-            log.info("refreshToken={}", refreshToken);
-
-            if (refreshToken != null && refreshTokenService.isValid(refreshToken)) {
-                try {
-                    UserClaims userClaims = jwtUtil.getUserClaims(refreshToken);
-                    TokenDto tokenDto = jwtUtil.createTokens(userClaims.id(), userClaims.role());
-                    log.info("access token 재발급");
-
-                    response.setHeader(HttpHeaders.SET_COOKIE, jwtUtil.createCookie(JwtUtil.REFRESH_TYPE, tokenDto.refreshToken()));
-                    response.sendRedirect(jwtUtil.getRedirectUrl(tokenDto.accessToken()));
-                } catch (Exception e2) {
-                    log.info("유효하지 않은 refresh token");
-                }
-            }
-        } finally {
-            filterChain.doFilter(request, response);
+            response.sendRedirect(jwtUtil.getRedirectUrl(tokenDto.accessToken()));
+        } catch (IOException e) {
+            log.info("redirect 실패");
+            throw new RuntimeException(e);
         }
+    }
+
+    private void handleAuthenticationFailure() {
+        log.info("인증 실패");
     }
 
 }
