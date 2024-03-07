2a10c712cb092eb44caa5626c38f1a3ae5de61da
전현민 <77275989+joyjhm@users.noreply.github.com>
2024-03-07 15:23:19 +0900

    feat: dto 예외 처리 기능 추가

--- src/main/java/com/project/sfoc/exception/CustomControllerAdvice.java
+++ src/main/java/com/project/sfoc/exception/CustomControllerAdvice.java
@@ -3,8 +3,10 @@ package com.project.sfoc.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
 
 @RestControllerAdvice
 @Slf4j
@@ -14,5 +16,15 @@ public class CustomControllerAdvice {
     public ResponseEntity<ErrorResponse> RefreshTokenException(BusinessException e) {
         log.error(e.error.getMessage());
         return new ResponseEntity<>(ErrorResponse.from(e.error), e.error.getHttpStatus());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> DtoValidException(MethodArgumentNotValidException e) {
+
+        e.getBindingResult()
+                .getFieldErrors().forEach(x -> log.error(x.getDefaultMessage()));
+
+        Error error = Error.INVALID_DTO;
+        return new ResponseEntity<>(ErrorResponse.from(error), error.getHttpStatus());
     }
 }
--- src/main/java/com/project/sfoc/exception/Error.java
+++ src/main/java/com/project/sfoc/exception/Error.java
@@ -10,10 +10,12 @@ public enum Error {
 
     EXPIRED_ACCESS_TOKEN(101, HttpStatus.BAD_REQUEST, "만료된 access token"),
     EXPIRED_REFRESH_TOKEN(102, HttpStatus.BAD_REQUEST, "만료된 refresh token"),
-    INVALID_TOKEN(103, HttpStatus.BAD_REQUEST, "유효하지 않은 token");
+    INVALID_TOKEN(103, HttpStatus.BAD_REQUEST, "유효하지 않은 token"),
+    INVALID_DTO(400, HttpStatus.BAD_REQUEST, "잘못된 dto 요청 입니다.");
 
     private final int code;
     private final HttpStatus httpStatus;
     private final String message;
+
 
 }
