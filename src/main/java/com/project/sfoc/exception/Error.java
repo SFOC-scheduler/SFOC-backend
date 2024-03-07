bf104f0c69826276e846bc1cbf4e89217331c16e
전현민 <77275989+joyjhm@users.noreply.github.com>
2024-03-07 15:59:49 +0900

    Refactor: 코드 리펙토링

--- src/main/java/com/project/sfoc/exception/Error.java
+++ src/main/java/com/project/sfoc/exception/Error.java
@@ -3,6 +3,7 @@ package com.project.sfoc.exception;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 @Getter
 @RequiredArgsConstructor
@@ -11,7 +12,7 @@ public enum Error {
     EXPIRED_ACCESS_TOKEN(101, HttpStatus.BAD_REQUEST, "만료된 access token"),
     EXPIRED_REFRESH_TOKEN(102, HttpStatus.BAD_REQUEST, "만료된 refresh token"),
     INVALID_TOKEN(103, HttpStatus.BAD_REQUEST, "유효하지 않은 token"),
-    INVALID_DTO(400, HttpStatus.BAD_REQUEST, "잘못된 dto 요청 입니다.");
+    INVALID_DTO(104, HttpStatus.BAD_REQUEST, "잘못된 dto 형식 입니다.");
 
     private final int code;
     private final HttpStatus httpStatus;
--- src/main/java/com/project/sfoc/exception/ErrorResponse.java
+++ src/main/java/com/project/sfoc/exception/ErrorResponse.java
@@ -1,5 +1,7 @@
 package com.project.sfoc.exception;
 
+import org.springframework.http.ResponseEntity;
+
 public record ErrorResponse(
         int code,
         String message
@@ -10,5 +12,9 @@ public record ErrorResponse(
 
     public static ErrorResponse from(Error error) {
         return ErrorResponse.of(error.getCode(), error.getMessage());
+    }
+
+    public static ResponseEntity<ErrorResponse> toResponseEntity (Error error) {
+        return new ResponseEntity<>(ErrorResponse.from(error), error.getHttpStatus());
     }
 }
