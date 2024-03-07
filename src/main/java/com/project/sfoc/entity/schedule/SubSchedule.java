0416bcfa9cfc6e1614ca48226e58b4bb7b87c253
PLT134340 <songwoo1015@gmail.com>
2024-03-07 17:17:09 +0900

    feat: 엔티티 필드 수정

--- src/main/java/com/project/sfoc/entity/schedule/SubSchedule.java
+++ src/main/java/com/project/sfoc/entity/schedule/SubSchedule.java
@@ -2,11 +2,13 @@ package com.project.sfoc.entity.schedule;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class SubSchedule {
 
@@ -14,14 +16,33 @@ public class SubSchedule {
     @Column(name = "sub_schedule_id")
     private Long id;
 
+    private String title;
+    private String memo;
+
+    private Boolean isEnableDday;
     private Boolean isSuccess;
 
-    private LocalDateTime startTime;
-
-    private LocalDateTime endTime;
+    private LocalDateTime startDateTime;
+    private LocalDateTime endDateTime;
 
     @ManyToOne
     @JoinColumn(name = "schedule_id")
     private Schedule schedule;
+
+    private SubSchedule(String title, String memo, Boolean isEnableDday, Boolean isSuccess,
+                        LocalDateTime startDateTime, LocalDateTime endDateTime, Schedule schedule) {
+        this.title = title;
+        this.memo = memo;
+        this.isEnableDday = isEnableDday;
+        this.isSuccess = isSuccess;
+        this.startDateTime = startDateTime;
+        this.endDateTime = endDateTime;
+        this.schedule = schedule;
+    }
+
+    public static SubSchedule of(String title, String memo, Boolean isEnableDday, Boolean isSuccess,
+                      LocalDateTime startDateTime, LocalDateTime endDateTime, Schedule schedule) {
+        return new SubSchedule(title, memo, isEnableDday, isSuccess, startDateTime, endDateTime, schedule);
+    }
 
 }
