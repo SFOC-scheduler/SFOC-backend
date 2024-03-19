98153c34800358de760eee6eebeff866acf5d3ea
PLT134340 <songwoo1015@gmail.com>
2024-03-19 21:42:23 +0900

    fix: JSON 컨버터 LocalDate 직렬화 및 병렬화 적용

--- src/main/java/com/project/sfoc/entity/schedule/PeriodRepeatConverter.java
+++ src/main/java/com/project/sfoc/entity/schedule/PeriodRepeatConverter.java
@@ -1,11 +1,24 @@
 package com.project.sfoc.entity.schedule;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
 import jakarta.persistence.AttributeConverter;
 import lombok.SneakyThrows;
 
+import java.time.LocalDate;
+
 public class PeriodRepeatConverter implements AttributeConverter<PeriodRepeat, String> {
-    private final ObjectMapper objectMapper = new ObjectMapper();
+    private final ObjectMapper objectMapper;
+
+    public PeriodRepeatConverter() {
+        objectMapper = new ObjectMapper();
+        JavaTimeModule javaTimeModule = new JavaTimeModule();
+        javaTimeModule.addSerializer(LocalDate.class, LocalDateSerializer.INSTANCE);
+        javaTimeModule.addDeserializer(LocalDate.class, LocalDateDeserializer.INSTANCE);
+        objectMapper.registerModule(javaTimeModule);
+    }
 
     @Override
     @SneakyThrows
