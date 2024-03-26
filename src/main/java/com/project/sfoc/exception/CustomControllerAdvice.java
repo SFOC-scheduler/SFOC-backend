58837546795530c6a480b09dbe30f62129be94c7
PLT134340 <songwoo1015@gmail.com>
2024-03-26 20:35:01 +0900

    fix:

--- src/main/java/com/project/sfoc/exception/CustomControllerAdvice.java
+++ src/main/java/com/project/sfoc/exception/CustomControllerAdvice.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingRequestCookieException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -33,4 +34,11 @@ public class CustomControllerAdvice {
         log.error("적절하지 않은 형식입니다.");
         return ErrorResponse.toResponseEntity(Error.INVALID_DTO);
     }
+
+    @ExceptionHandler(MissingRequestCookieException.class)
+    public ResponseEntity<ErrorResponse> missingRequestCookieException(HttpMessageNotReadableException e) {
+        log.error("refresh token이 전달되지 않았습니다.");
+        return ErrorResponse.toResponseEntity(Error.INVALID_TOKEN);
+    }
+
 }
